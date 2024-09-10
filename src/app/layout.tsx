92015d8d2a2fcdc1cadb9410f032602275172acd
Abhishek Mahato <abhishek.mahato98258@gmail.com>
2024-09-10 10:07:17 +0545

    add seo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,9 +14,12 @@ const geistMono = localFont({
 });
 
 export const metadata: Metadata = {
-  title: "Abhishek Mahato | FullStack WebDeveloper",
+  title: "Abhishek Mahato",
   description:
     "Portfolio of Abhishek Mahato a FullStack WebDeveloper from Nepal. Is skilled in React, NextJs, TailwindCSS, NodeJs, ExpressJs, MongoDB, and other web technologies. Islington College Graduate.",
+  keywords:
+    "Abhishek Mahato, abhishek-mahato, abishek mahato, abishekh mahato, FullStack Developer, Web Developer, React Developer, NextJs Developer, TailwindCSS Developer, NodeJs Developer, ExpressJs Developer, MongoDB Developer, Web Technologies, Islington College Graduate, innovate, naxa, Nepal, Kathmandu",
+  authors: [{ name: "Abhishek Mahato" }],
 };
 
 export default function RootLayout({
